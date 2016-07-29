d14b8efd76d19551583b462cae6b7e782249b518
Davor Babić <davor@davor.se>
2016-07-29 07:14:49 +0200

    changed indentation style to linux

--- src/main.c
+++ src/main.c
@@ -3,28 +3,31 @@
 
 #define LIGHT_SENSOR_PATH "/sys/devices/platform/applesmc.768/light"
 
-void sensor_err_msg() {
-	fprintf(stderr, "Failed to read the light sensor value.\n");
-	exit(1);
+void sensor_err_msg()
+{
+		fprintf(stderr, "Failed to read the light sensor value.\n");
+		exit(1);
 }
 
-int sensor_value() {
-	FILE *fp;
-	if ((fp = fopen(LIGHT_SENSOR_PATH, "r")) == NULL) {
-		sensor_err_msg();
-	}
+int sensor_value()
+{
+		FILE *fp;
+		if ((fp = fopen(LIGHT_SENSOR_PATH, "r")) == NULL) {
+				sensor_err_msg();
+		}
 
-	char line[16];
-	fgets(line, 16, fp);
-	if (line == NULL) {
-		sensor_err_msg();
-	}
-	int value = atoi(line+1);
-	fclose(fp);
-	return value;
+		char line[16];
+		fgets(line, 16, fp);
+		if (line == NULL) {
+				sensor_err_msg();
+		}
+		int result = atoi(line+1);
+		fclose(fp);
+		return result;
 }
 
-int main() {
-	printf("current light sensor level: %i\n", sensor_value());
-	return 0;
+int main()
+{
+		printf("current light sensor level: %i\n", sensor_value());
+		return 0;
 }
