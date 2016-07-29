a070c1d9f7c7f59855504a122e031426a304c3e4
Davor Babić <davor@davor.se>
2016-07-29 14:16:33 +0200

    adds function set_backlight()

--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
+/* calvados -- daemon for automatically setting laptop backlight levels */
+/* Copyright (C) 2016 Davor Babić <davor@davor.se> */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #define LIGHT_SENSOR_PATH "/sys/devices/platform/applesmc.768/light"
+#define BACKLIGHT_PATH "/sys/class/backlight/intel_backlight/brightness"
 
 void sensor_err_msg()
 {
@@ -26,8 +43,22 @@ int sensor_value()
 	return result;
 }
 
+void set_backlight(int level)
+{
+	FILE *fp;
+	if ((fp = fopen(BACKLIGHT_PATH, "w")) == NULL) {
+		fprintf(stderr, "Unable to find backlight ACPI.\n");
+		exit(1);
+	}
+
+	fprintf(fp, "%i", level);
+
+	fclose(fp);
+}
+
 int main()
 {
 	printf("current light sensor level: %i\n", sensor_value());
+	set_backlight(1800);
 	return 0;
 }
