83d6fb7b633e701db7e84b8cea336139ab5b46b9
Davor Babić <davor@davor.se>
2016-07-29 07:12:05 +0200

    adds ability to read the light sensor

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #define LIGHT_SENSOR_PATH "/sys/devices/platform/applesmc.768/light"
 
+void sensor_err_msg() {
+	fprintf(stderr, "Failed to read the light sensor value.\n");
+	exit(1);
+}
+
+int sensor_value() {
+	FILE *fp;
+	if ((fp = fopen(LIGHT_SENSOR_PATH, "r")) == NULL) {
+		sensor_err_msg();
+	}
+
+	char line[16];
+	fgets(line, 16, fp);
+	if (line == NULL) {
+		sensor_err_msg();
+	}
+	int value = atoi(line+1);
+	fclose(fp);
+	return value;
+}
+
 int main() {
-	printf("hello, world!");
+	printf("current light sensor level: %i\n", sensor_value());
 	return 0;
 }
